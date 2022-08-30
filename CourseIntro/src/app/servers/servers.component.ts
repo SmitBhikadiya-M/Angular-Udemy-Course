bade0d2d679443067909759d1003b5e1da8cc2e9
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-08-30 15:09:15 +0530

    Learn: String Interpolation, Property Binding, Event Binding, Two Data Binding

--- CourseIntro/src/app/servers/servers.component.ts
+++ CourseIntro/src/app/servers/servers.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers.',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = true;
+  serverCreationStatus = 'No server was created';
+  serverName = 'TestServer';
+  constructor() { 
+    setTimeout(()=>{
+      this.allowNewServer = false;
+    }, 2000)
+   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = `${this.serverName} was created`;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
