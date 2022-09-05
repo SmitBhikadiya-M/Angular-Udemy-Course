1dea89426651b478986b83c6669d717b9b90de3c
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-09-05 12:12:13 +0530

    Explore: HostListener & HostBinding

--- DirectivesInDeep/src/app/app.module.ts
+++ DirectivesInDeep/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './directives/basic-highlight.directive';
+import { BetterHighlightDirective } from './directives/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule
--- DirectivesInDeep/src/app/directives/basic-highlight.directive.ts
+++ DirectivesInDeep/src/app/directives/basic-highlight.directive.ts
@@ -4,7 +4,7 @@ import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
     selector: '[appBasicHighlight]'
 })
 export class BasicHighlightDirective implements OnInit{
-    constructor( private elementRef: ElementRef, private renderer: Renderer2 ){}
+    constructor( private elementRef: ElementRef){}
 
     ngOnInit(){
         this.elementRef.nativeElement.style.backgroundColor = 'green';
