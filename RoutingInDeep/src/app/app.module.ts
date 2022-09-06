6a3dc5b3b83a7d61003d6d4831858570dd4812ab
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-09-06 14:15:39 +0530

    Updated: Route For Params

--- RoutingInDeep/src/app/app.module.ts
+++ RoutingInDeep/src/app/app.module.ts
@@ -14,8 +14,10 @@ import { ServersService } from './servers/servers.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UserComponent },
-  { path: 'servers', component: ServersComponent }
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id/:name', component: UserComponent },
+  { path: 'servers', component: ServersComponent },
+  { path: '**', redirectTo: '/'}
 ]
 
 @NgModule({
--- RoutingInDeep/src/app/home/home.component.ts
+++ RoutingInDeep/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +8,12 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
   loadServers(){
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers'], { relativeTo: this.route });
   }
 }
