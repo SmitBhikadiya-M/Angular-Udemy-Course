433d32f636d86c3bcfd193ae1f31064040d19443
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-09-08 16:33:29 +0530

    Covert Shopping List Form to Template Driven Form

--- CourseProject/src/app/shared/services/shopping-list.service.ts
+++ CourseProject/src/app/shared/services/shopping-list.service.ts
@@ -4,6 +4,7 @@ import { Ingredient } from "../models/ingredient.model";
 export class ShoppingListService{
 
     ingredientChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -14,6 +15,10 @@ export class ShoppingListService{
         return this.ingredients.slice();
       }
 
+      getIngredient(index: number){
+        return this.ingredients[index];
+      }
+
       addIngredient(ingredient: Ingredient){
         this.ingredients.push(ingredient);
         this.ingredientChanged.next(this.ingredients.slice());
@@ -22,5 +27,15 @@ export class ShoppingListService{
       addIngredients(ingredient: Ingredient[]){
         this.ingredients.push(...ingredient);
         this.ingredientChanged.next(this.ingredients.slice());
+      }
+
+      updateIngredient(index: number, newIngredient: Ingredient){
+        this.ingredients[index] = newIngredient;
+        this.ingredientChanged.next(this.ingredients.slice());
+      }
+
+      deleteIngredient(index: number){
+        this.ingredients.splice(index,1);
+        this.ingredientChanged.next(this.ingredients.slice())
       }
 }
