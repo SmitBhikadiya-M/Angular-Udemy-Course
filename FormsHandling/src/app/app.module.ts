813671cd033b02eabccbdd50e0812b4326569fcb
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-09-08 10:51:49 +0530

    Outcomes: FormArray Dynamic, FormGroup, FormControl

--- FormsHandling/src/app/app.module.ts
+++ FormsHandling/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -16,6 +16,7 @@ import { TemplateDrivenFormComponent } from './template-driven-form/template-dri
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FormsHandling/src/app/reactive-form/reactive-form.component.ts
+++ FormsHandling/src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormComponent implements OnInit {
   genders = ['male', 'female'];
+  signupForm!: FormGroup;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, [ Validators.required ]),
+        'email': new FormControl(null, [ Validators.email, Validators.required ]),
+      }),
+      'gender': new FormControl('male', [ Validators.required ]),
+      'hobbies': new FormArray([])
+    });
+  }
+
+  onSubmit(){
+    console.log(this.signupForm.value);
+  }
+
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray> this.signupForm.get('hobbies')).push(control);
+  }
+ get hobbyControls(){
+    return (<FormArray> this.signupForm.get('hobbies')).controls;
   }
 
 }
