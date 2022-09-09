96951ec99e4473d0e11b394e988f582182917dc8
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-09-09 17:09:16 +0530

    Create Service & Fixed Issues With HttpError

--- HttpRequestsWithFirebase/src/app/app.component.ts
+++ HttpRequestsWithFirebase/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { map, pipe } from 'rxjs';
+import { Post } from './models/post.model';
+import { PostService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -7,22 +10,45 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
+  isFetching = true;
+  error = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(private postService: PostService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.onFetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    console.log(postData);
+  onCreatePost(postData: Post) {
+    this.postService.createAndStorePost(postData.title, postData.content).subscribe((res) => {
+      console.log(res);
+    });
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.isFetching = true;
+    this.error = null;
+    this.postService.fetchPosts().subscribe({
+      next: (posts: Post[]) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      error: (err: HttpErrorResponse) => {
+        this.postService.errorSubj.next(err.message);
+        this.error = err.message;
+        this.isFetching = false;
+      }
+    })
   }
 
   onClearPosts() {
-    // Send Http request
+    this.postService.deletePosts().subscribe((res) => {
+      this.loadedPosts = [];
+    })
+  }
+
+  onHandleError() {
+    this.error = null;
   }
 }
