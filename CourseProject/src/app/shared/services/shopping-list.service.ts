d050a4140347bc8ed6205da9fdd21c842bd4a71b
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-09-13 14:44:43 +0530

    Spliting out all the required features module with saperate routings module

--- CourseProject/src/app/shared/services/shopping-list.service.ts
+++ CourseProject/src/app/shared/services/shopping-list.service.ts
@@ -1,6 +1,10 @@
+import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 import { Ingredient } from "../models/ingredient.model";
 
+@Injectable({
+  providedIn: 'root'
+})
 export class ShoppingListService{
 
     ingredientChanged = new Subject<Ingredient[]>();
