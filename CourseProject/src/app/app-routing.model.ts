c514ceb327eead16fb717fa4b5d4643ccf127951
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-09-13 15:51:27 +0530

    Modified For Lazy Loading Implementation for modules

--- CourseProject/src/app/app-routing.model.ts
+++ CourseProject/src/app/app-routing.model.ts
@@ -1,17 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AuthGuard } from "./auth/auth-gaurd.service";
-import { AuthComponent } from "./auth/auth.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipesResolverService } from "./recipes/recipes-resolver.service";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'auth', component: AuthComponent }
+  {
+    path: 'recipes',
+    loadChildren: () => import('./recipes/recipes-rotuing.module').then(mod => mod.RecipesRoutingModule)
+  },
+  {
+    path: 'shopping-list',
+    loadChildren: () => import('./shopping-list/shopping-list.module').then(mod => mod.ShoppingListModule)
+  },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
 ]
 
 @NgModule({
--- CourseProject/src/app/app.module.ts
+++ CourseProject/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http' 
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header/header.component';
@@ -23,8 +23,6 @@ import { AuthModule } from './auth/auth.module';
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
-    RecipesModule,
-    ShoppingListModule,
     AuthModule
   ],
   providers: [
