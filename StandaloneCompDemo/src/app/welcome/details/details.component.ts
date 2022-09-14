21989821cebab5ac23d60c078b1dbd2c375a06fa
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-09-14 11:12:43 +0530

    Intial Project Setup: Standalone Component

---
+++ StandaloneCompDemo/src/app/welcome/details/details.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AnalyticsService } from 'src/app/shared/analytics.service';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@Component({
+  standalone: true,
+  selector: 'app-details',
+  imports: [SharedModule],
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css'],
+})
+export class DetailsComponent {
+  constructor(private analyticsService: AnalyticsService) {}
+
+  onClick() {
+    this.analyticsService.registerClick();
+  }
+}
