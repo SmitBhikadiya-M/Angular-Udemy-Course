f8a9ed0dd164eebbcd36c223c3e6437c2fbec758
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-09-14 11:26:22 +0530

    Removed NgModule Which is replaced By Standalone Cmponents.

--- StandaloneCompDemo/src/app/welcome/details/details.component.ts
+++ StandaloneCompDemo/src/app/welcome/details/details.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { AnalyticsService } from 'src/app/shared/analytics.service';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { HighlightDirective } from 'src/app/shared/highlight.directive';
 
 @Component({
   standalone: true,
   selector: 'app-details',
-  imports: [SharedModule],
+  imports: [HighlightDirective],
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent {
   constructor(private analyticsService: AnalyticsService) {}
-
   onClick() {
     this.analyticsService.registerClick();
   }
